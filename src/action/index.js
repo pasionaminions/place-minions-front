6a74772e8ffea5d2358e432cdcd54d1e5ca89b33
BetoxX98 <50861438+BetoxX98@users.noreply.github.com>
2020-02-11 13:00:42 +0100

    Actions + reducer + thunks

---
+++ src/action/index.js
@@ -0,0 +1,13 @@
+import { DRAW_POINT, GET_POINTS, GET_COLORS } from "./action-types";
+
+export function drawPoints(json) {
+    return {type: DRAW_POINT, payload: json}
+}
+
+export function getPoints(json) {
+    return {type: GET_POINTS, payload: json}
+}
+
+export function getColors(json) {
+    return {type: GET_COLORS, payload: json}
+}
