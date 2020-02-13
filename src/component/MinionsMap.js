5aa4b797e94a70f3fde6029203a6c1563976e005
BetoxX98 <50861438+BetoxX98@users.noreply.github.com>
2020-02-13 12:49:49 +0100

    FrontCasi acabado

--- src/component/MinionsMap.js
+++ src/component/MinionsMap.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { getPoints, getColors, drawPoint } from "../thunks";
 import { Button, Input } from 'reactstrap';
-
+import '../App.css';
 
 const mapDispatchToProps = {
     getColors, getPoints, drawPoint
@@ -33,6 +33,14 @@ componentDidMount() {
         this.setState({ [event.target.name]: event.target.value });
     }
 
+    selectPoint = (x,y, event) => {
+        event.target.style.border = "solid 1px black";
+        this.setState({
+            x: x,
+            y: y
+        })
+    }
+
     loadPage = () => {
         this.props.getPoints().then(() => this.setState({loaded:true}))
     }
@@ -48,21 +56,24 @@ componentDidMount() {
         if  (!this.state.loaded) return <div>Loading...</div>;
         return (
             <>  
-                <div style={{display:"flex", justifyContent: "center"}}>
-                    <div style={{width:700, margin:13}}>
+                <div style={{display:"flex", justifyContent: "center", backgroundColor: "#e68422"}}>
+                    <h1 style={{margin:10,color:"white", letterSpacing: 10, textShadow: "4px 4px 5px rgba(0,0,0,0.55)"}}>
+                        PASIONA<br/>L<br/>A<br/>C<br/>E
+                        </h1>
+                    <div style={{width:600, margin:13, boxShadow: "9px 10px 5px 0px rgba(0,0,0,0.55)"}}>
                         {this.props.Points.map((x, indx)=>{
                             return <div key={indx} style={{display:"flex"}}>
                                 {x.map((y,indy)=>{
-                                    return <div key={indx+"-"+indy}style={{width:7, height:7, border: "1px solid", backgroundColor: this.props.Colors[y]}}></div>
+                                    return <div key={indx+"-"+indy} id={indx+"-"+indy} className="Point" style={{width:6, height:6, backgroundColor: this.props.Colors[y]}} onClick={(event)=>{this.selectPoint(indy, indx, event)}}></div>
                                 })}  
                             </div>   
                         })}
                     </div>
-                    <div style={{width:200, display:"flex", flexWrap:"wrap", height:600, margin: 13, border: "1px solid"}}>
+                    <div style={{width:200, display:"flex", flexWrap:"wrap", height:600, margin: 13, backgroundColor:"white", boxShadow: "9px 10px 5px 0px rgba(0,0,0,0.55)"}}>
                         <Input style={{margin:10, width:180}} type="number" name="x" value={this.state.x} onChange={this.handleChange}></Input>
                         <Input style={{margin:10, width:180}} type="number" name="y" value={this.state.y} onChange={this.handleChange}></Input>
                         {this.props.Colors.map((x,i)=>{
-                            return <Button name={x} key={i} style={{margin:10, marginTop:4, width:180, backgroundColor:x}} onClick={()=>{this.send(i)}}></Button>
+                            return <Button name={x} key={i} style={{margin:10, marginTop:2, width:180, backgroundColor:x}} onClick={()=>{this.send(i)}}></Button>
                         })}
                     </div>
                 </div>
